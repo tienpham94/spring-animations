111991021633cc53bc3a26b5b2abc2bf6aa045af
Tien Pham <tiendev94@gmail.com>
2019-05-22 21:07:17 +0300

    Basic interpolation

--- src/Toggle.js
+++ src/Toggle.js
@@ -3,14 +3,22 @@ import { useSpring, animated } from "react-spring";
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
-  const fade = useSpring({
-    opacity: isToggled ? 1 : 0,
-    color: isToggled ? "tomato" : "green"
+  const { y, color } = useSpring({
+    // opacity: isToggled ? 1 : 0,
+    color: isToggled ? "tomato" : "green",
+    y: isToggled ? 0 : -50
   });
 
   return (
     <div>
-      <animated.h1 style={fade}>Hello</animated.h1>
+      <animated.h1
+        style={{
+          color,
+          transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`)
+        }}
+      >
+        Hello
+      </animated.h1>
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
     </div>
   );
